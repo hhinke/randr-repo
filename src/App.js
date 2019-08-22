5b6ec116f945ca2a0198cc9bd8b486623ee76479
Henrique S. Hinke <hhinke@gmail.com>
2019-08-22 01:02:23 -0300

    First commit with initial version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [repo, setRepo] = useState(null);
+
+  useEffect(() => {
+    async function loadRandomRepo() {
+      // generate random repo id
+      var repoId = Math.floor((Math.random() * 50000) + 1);
+      const reposResponse = await axios.get(`https://api.github.com/repositories?since=${repoId}`);
+
+      if(reposResponse.data.length > 0) {        
+        // read only first item of response
+        const { full_name, description, html_url, languages_url, owner: { avatar_url },  } = reposResponse.data[0];
+        const languageResponse = await axios.get(languages_url);
+
+        setRepo({
+          full_name,
+          url: html_url,
+          description,
+          avatar_url,
+          languages: Object.keys(languageResponse.data)
+        });
+      }
+    }
+
+    loadRandomRepo();
+  },[]); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="main-container">
+      { repo !== null ? (
+        <p> 
+          <img src={repo.avatar_url} alt={repo.full_name} />
+          <strong>{repo.full_name}</strong>
+          <p>{repo.description}</p>
+
+          <div className="buttons">
+            <a href={repo.url}>
+              <button type="button">Go!</button>
+            </a>
+          </div>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ) : (
+        <div className="loading">Loading...</div>
+      )}
     </div>
   );
 }
-
-export default App;
